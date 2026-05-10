ed2f9a953e00e73773bad15d86b8a452a8311aa2
ceekay-munshot <ceekay@muns.io>
2026-05-11 01:46:39 +0530

    chore: rotate Muns bearer token

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 //
 // NOTE: this is a static site, so anything placed here ships to the browser.
 // Use a token scoped to read-only stock search.
-window.MUNS_BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ZWE5ZGMyYi0xZDBmLTQ2MzctOGE2Ny0wM2VhNzFmMGYyY2YiLCJlbWFpbCI6Im5hZGFtc2FsdWphQGdtYWlsLmNvbSIsIm9yZ0lkIjoiMSIsImF1dGhvcml0eSI6ImFkbWluIiwiaWF0IjoxNzc3OTgzNTUzLCJleHAiOjE3Nzg0MTU1NTN9.IQKdGF0H3E_KzCy5h5dyTAIFgSMkbHQ5PEtNjtEVY_c';
+window.MUNS_BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ZWE5ZGMyYi0xZDBmLTQ2MzctOGE2Ny0wM2VhNzFmMGYyY2YiLCJlbWFpbCI6Im5hZGFtc2FsdWphQGdtYWlsLmNvbSIsIm9yZ0lkIjoiMSIsImF1dGhvcml0eSI6ImFkbWluIiwiaWF0IjoxNzc4NDM0MDY4LCJleHAiOjE3Nzg4NjYwNjh9.uqQ3uVj2JcwpF3eoaZ2VZ5kMaa2U1Pm47nC9ejHo1rQ';
 
 // Optional override of the API base. Leave as-is for production.
 window.MUNS_API_BASE = 'https://birdnest.muns.io';
